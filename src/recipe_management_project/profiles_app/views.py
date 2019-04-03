be05a70816100ff65be1d26550563a5d9561b5b6
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-04-03 18:32:27 +0500

    Added permission and Authentication for UserProfileViewset

--- src/recipe_management_project/profiles_app/views.py
+++ src/recipe_management_project/profiles_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from . import models
 from . import serializers
+from . import permissions 
 
 # Create your views here.
 
@@ -12,7 +14,8 @@ class UserProfilesViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (permissions.UpdateOwnProfile,)
+    
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     # filter_backends = (filters.SearchFilter,)
     # search_fields = ('name', 'email',)
