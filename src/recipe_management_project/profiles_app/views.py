00c85af44e7d4041bea028c857d70688a84a7533
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-04-03 16:34:37 +0500

    Added profile listing and creating feature

--- src/recipe_management_project/profiles_app/views.py
+++ src/recipe_management_project/profiles_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from . import models
+from . import serializers
+
 # Create your views here.
+
+class UserProfilesViewSet(viewsets.ModelViewSet):
+    """handles creating, reading and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (permissions.UpdateOwnProfile,)
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ('name', 'email',)
