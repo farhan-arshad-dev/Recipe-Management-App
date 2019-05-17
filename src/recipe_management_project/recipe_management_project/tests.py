c8f16f27220749400590a52af4dc13179f86e949
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-05-17 16:46:43 +0500

    Write test using TDD
    - First write test to subtract two number.
    - After that create the method to subtract the two numbers

--- src/recipe_management_project/recipe_management_project/tests.py
+++ src/recipe_management_project/recipe_management_project/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from recipe_management_project.calc import add
+from recipe_management_project.calc import add, substract
 # Create your tests here.
 
 
@@ -9,3 +9,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         self.assertEqual(add(3, 8), 11)
+
+    def test_substract_numbers(self):
+        """Test that values are substracted and returned"""
+        self.assertEqual(substract(5, 11), 6)
