9248650b1c46873d213e0eaaa71088e5a8f3204f
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-05-30 16:31:32 +0500

    Update path for basic unit tests

--- src/recipe_management_project/recipe_management_project/tests/test_basic.py
+++ src/recipe_management_project/recipe_management_project/tests/test_basic.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
 from recipe_management_project.calc import add, substract
-# Create your tests here.
 
 
+# Basic unit test for the project 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
@@ -13,3 +13,4 @@ class CalcTests(TestCase):
     def test_substract_numbers(self):
         """Test that values are substracted and returned"""
         self.assertEqual(substract(5, 11), 6)
+
