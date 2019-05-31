903e64963c316029ec3a80d84858991fc9aa5453
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-05-31 15:31:00 +0500

    Add postgres database unit test
    - Added wait_for_db management command

--- src/recipe_management_project/recipe_management_project/tests/test_basic.py
+++ src/recipe_management_project/recipe_management_project/tests/test_basic.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from recipe_management_project.calc import add, substract
 
 
-# Basic unit test for the project 
+# Basic unit test for the project
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
@@ -13,4 +13,3 @@ class CalcTests(TestCase):
     def test_substract_numbers(self):
         """Test that values are substracted and returned"""
         self.assertEqual(substract(5, 11), 6)
-
