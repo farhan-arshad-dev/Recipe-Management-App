7f1d0c0e7987a817dcf10df28b1dd97367ce26e9
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-08-05 17:54:58 +0500

    Add view files inside views python package

--- src/recipe_management_project/api/urls.py
+++ src/recipe_management_project/api/urls.py
@@ -4,18 +4,18 @@ Module to define the url for the api application.
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
-from . import user_profile_views, recipe_views, followers_views
+from .views import user_profile, recipe, followers
 
 router = DefaultRouter()
 # Use this configration to hide the API root browsable
 # router = SimpleRouter()
 
-router.register('profile', user_profile_views.UserProfilesViewSet)
-router.register('login', user_profile_views.LoginViewSet, base_name='login')
-router.register('recipe', recipe_views.RecipeViewSet)
-router.register('follow', followers_views.FollowingViewSet)
+router.register('profile', user_profile.UserProfilesViewSet)
+router.register('login', user_profile.LoginViewSet, base_name='login')
+router.register('recipe', recipe.RecipeViewSet)
+router.register('follow', followers.FollowingViewSet)
 
 urlpatterns = [
-    url('change_password', user_profile_views.ChangePasswordView.as_view()),
+    url('change_password', user_profile.ChangePasswordView.as_view()),
     url(r'', include(router.urls))
 ]
