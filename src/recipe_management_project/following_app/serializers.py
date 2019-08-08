53d4b6f8f2f25b8a8c833168039554968c68e4f2
farhan-arshad-dev <farhan.arshad@arbisoft.com>
2019-08-08 20:12:06 +0500

    Added user info in followed user list

--- src/recipe_management_project/following_app/serializers.py
+++ src/recipe_management_project/following_app/serializers.py
@@ -3,13 +3,16 @@ Deinfe serializers mechanism for “translating” Django models into other
 formats. Usually text-based formats (e.g json)
 """
 from rest_framework import serializers
+
+from profiles_app.serializers import UserProfileSerializer
+
 from . import models
 
 
 class UserFollowingSerializer(serializers.ModelSerializer):
     """Serializer for user following item"""
 
+    following_to = UserProfileSerializer()
     class Meta:
         model = models.FollowingModel
-        fields = ('id', 'following_by', 'following_to', 'created_on')
-        extra_kwargs = {'following_by': {'read_only': True}}
+        fields = ('id', 'following_to', 'created_on')
